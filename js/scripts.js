3fad33e666ca8a737c6e241a0cb7883d2afe1dcb
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-19 15:12:32 +0200

    Zadanie 9.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function() { 
+	$('body').css('text-align', 'center');
 
+    $('span:odd').css('color', 'red');
+    $('span:odd')
+    $('#mainDiv').css('background', 'pink');
+
+    var paragraphs = $('p');
+
+	paragraphs.each(function(index, elem) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+		$(elem).append(button); 
+	});
+
+	$('button').click(function() {
+		alert($(this).attr('data-tmp'));
+		$(this).fadeTo('slow', '0.25');
+	});
+
+	$('button').mouseover(function() {
+		$(this).css('background', 'white');
+	});
+
+	$('#mainDiv').dblclick(function() {
+		$(this).fadeOut('fast');
+	});
 });

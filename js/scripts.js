83460476dba530b40ae6b5a19b94888eda5e5ea3
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-24 12:13:08 +0200

    Zadanie 9.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
-$(document).ready(function() { 
+$(function(){
+    var carouselList = $("#carousel ul");
+
+	setInterval(changeSlide, 5000);
+
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 1200, moveFirstSlide());
+	}
+    
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+
+	function moveSlideBack() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.before(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+	$('#prevButton').click(function() {
+		carouselList.animate({'marginleft':-400}, 500, moveSlideBack());
+	});
+
+	$('#nextButton').click(function() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide());
+	});
 
 });
